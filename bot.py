c841350f63b560929c822383ae7cf94cfdf488e6
Mavhawk64 <mavberk@gmail.com>
2021-10-27 08:34:27 -0500

    Added starter files
    Used starter code from CoinMarketCap API website. Made some adjustments to call the api key via a file.

---
+++ bot.py
@@ -0,0 +1,31 @@
+with open('api-key.txt') as f:
+	coinmarketcap_key = f.readlines()[0] # something like 'a123b45c-6de7-8fg9-01h2-3ij4kl56m789'
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+
+url = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest' # testing
+# url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest' # prod
+parameters = {
+  'start':'1',
+  'limit':'5000',
+  'convert':'USD'
+}
+headers = {
+  'Accepts': 'application/json',
+  'X-CMC_PRO_API_KEY': coinmarketcap_key,
+}
+
+session = Session()
+session.headers.update(headers)
+print(session.headers)
+f2 = open("sample_output.json", "w")
+try:
+  response = session.get(url, params=parameters)
+  data = json.loads(response.text)
+  print(data)
+  f2.write(json.dumps(data, indent=2))
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+  f2.write(e)
+
+
